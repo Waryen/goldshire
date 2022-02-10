4293141f795fcb16083875c4feb064fb38c1a80a
Waryen <jonathangomand@gmail.com>
2022-02-10 22:34:36 +0100

    refactor(character): remove from field in character generator

--- src/generators/characters.ts
+++ src/generators/characters.ts
@@ -2,11 +2,16 @@ import { characters, quotes } from '../models/index';
 import shuffle from 'lodash.shuffle';
 
 export const generateCharacter = () => {
-	const shuffledCharacters = shuffle(characters);
-	const character = shuffledCharacters[0];
-	const quotesFrom = quotes.filter((quote) => quote.from == character.id);
-	return {
-		character,
-		quotes: quotesFrom,
-	};
+  const shuffledCharacters = shuffle(characters);
+  const character = shuffledCharacters[0];
+  const quotesFrom = quotes
+    .filter((quote) => quote.from == character.id)
+    .map((quote) => {
+      const { from, ...rest } = quote;
+      return rest;
+    });
+  return {
+    character,
+    quotes: quotesFrom,
+  };
 };
