19fabb6c027f6cc074c2c4780ba007a8fe13193d
Waryen <jonathangomand@gmail.com>
2022-02-11 22:06:35 +0100

    chore(data): add more data

--- src/models/characters.ts
+++ src/models/characters.ts
@@ -1,12 +1,28 @@
 import { Character } from '../types';
 
 export const characters: Character[] = [
-	{
-		id: 1,
-		name: 'Sylvanas Windrunner',
-	},
-	{
-		id: 2,
-		name: 'Illidan Stormrage',
-	},
+  {
+    id: 1,
+    name: 'Sylvanas Windrunner',
+  },
+  {
+    id: 2,
+    name: 'Illidan Stormrage',
+  },
+  {
+    id: 3,
+    name: 'Bolvar Fordragon',
+  },
+  {
+    id: 4,
+    name: 'Therenas Menethil',
+  },
+  {
+    id: 5,
+    name: 'Ragnaros',
+  },
+  {
+    id: 6,
+    name: 'Chen Stormstout',
+  },
 ];
--- src/models/quotes.ts
+++ src/models/quotes.ts
@@ -1,24 +1,54 @@
 import { Quote } from '../types';
 
 export const quotes: Quote[] = [
-	{
-		id: 1,
-		text: 'Ours is a cycle of hatred. Alliances forged and broken. We have paid the price for sharing this world, and we have forgotten what makes us strong.',
-		from: 1,
-	},
-	{
-		id: 2,
-		text: 'Icecrown... a monument to our suffering. The veil between life and death. Where a usurper sits on a Frozen Throne. But no king rules forever.',
-		from: 1,
-	},
-	{
-		id: 3,
-		text: 'Imprisoned for ten thousand years. Banished from my own homeland. And now, you dare enter my realm. You are not prepared!',
-		from: 2,
-	},
-	{
-		id: 4,
-		text: 'The hunter is nothing without the hunt.',
-		from: 2,
-	},
+  {
+    id: 1,
+    text: 'Ours is a cycle of hatred. Alliances forged and broken. We have paid the price for sharing this world, and we have forgotten what makes us strong.',
+    from: 1,
+  },
+  {
+    id: 2,
+    text: 'Icecrown... a monument to our suffering. The veil between life and death. Where a usurper sits on a Frozen Throne. But no king rules forever.',
+    from: 1,
+  },
+  {
+    id: 3,
+    text: 'Imprisoned for ten thousand years. Banished from my own homeland. And now, you dare enter my realm. You are not prepared!',
+    from: 2,
+  },
+  {
+    id: 4,
+    text: 'The hunter is nothing without the hunt.',
+    from: 2,
+  },
+  {
+    id: 5,
+    text: 'Tell them only that the Lich King is dead and that Bolvar Fordragon, died with him!',
+    from: 3,
+  },
+  {
+    id: 6,
+    text: 'My son, the day you were born, the very forests of Lordaeron whispered the name… Arthas.',
+    from: 4,
+  },
+  {
+    id: 7,
+    text: 'BY FIRE BE PURGED!',
+    from: 5,
+  },
+  {
+    id: 8,
+    text: 'TOO SOON! YOU HAVE AWAKENED ME TOO SOON, EXECUTUS!',
+    from: 5,
+  },
+  {
+    id: 9,
+    text: 'For my kind, the true question is: What is worth fighting for?',
+    from: 6,
+  },
+  {
+    id: 10,
+    text: 'Nothing lasts.',
+    from: 1,
+  },
 ];
